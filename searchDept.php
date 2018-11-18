584e50a6a4b9981908a425367461b5d844ef0e20
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-17 20:34:06 -0600

    Update searchDept.php

--- searchDept.php
+++ searchDept.php
@@ -1,30 +1,24 @@
 <?php
-echo '<link rel="stylesheet" type = "text/css" href="../../css/resultStyle.css">';
-echo '<link rel="stylesheet" type = "text/css" href="../../css/pageStyle.css">';
-include '../headerFooter.php';
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "telmon_database";
+echo '<link rel="stylesheet" type = "text/css" href="../css/resultStyle.css">';
+echo '<link rel="stylesheet" type = "text/css" href="../css/pageStyle.css">';
+include 'headerFooter.php';
+require 'open-db.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname); //open connection
-
-if ($conn->connect_error) { //error if no connection
-    die("Connection failed: " . $conn->connect_error);
-} 
 /*Department info*/
 $Dept_ID= $_POST['dNo'];
-$Dept_Name= $_POST["dName"];
-$Office_Addr= $_POST["dAddr"];
-$Office_Phone= $_POST["dPhone"];
-
-//if (isset($_POST['search'])) //, $_POST['dName'], $_POST['dAddr'], $_POST['dPhone'])) //check for info
+$Dept_Name= $_POST['dName'];
+$Office_Addr= $_POST['dAddr'];
+$Office_Phone= $_POST['dPhone'];
 	
-		$result= mysqli_query($conn, "SELECT * FROM department where Dept_ID=$Dept_ID");
-		if (!$result) {
+		$id= mysqli_query($conn, "SELECT * FROM department where Dept_ID=$Dept_ID");
+		$name= mysqli_query($conn, "SELECT * FROM department where Dept_Name like '%$Dept_Name%'");
+		$addr= mysqli_query($conn, "SELECT * FROM department where Office_Addr like '%$Office_Addr%'");
+		$phone= mysqli_query($conn, "SELECT * FROM department where Office_Phone like '%$Office_Phone%'");
+	
+		$id= mysqli_query($conn, "SELECT * FROM department where Dept_ID=$Dept_ID");
+		if (!$id) {
 		printf("Error: %s\n", mysqli_error($conn));
 		exit();}
-
 		/*where Dept_ID = $Dept_ID;
 		or Dept_Name=$Dept_Name 
 		or Office_Addr=$Office_Addr 
@@ -38,7 +32,7 @@ $Office_Phone= $_POST["dPhone"];
 			<th>Office Phone</th>
 		</tr>';
 		
-		while ($row = mysqli_fetch_array($result)) {
+		while ($row = mysqli_fetch_array($id)) {
 			echo "<tr>";
 			echo "<td>" . $row['Dept_ID'] . "</td>";
 			echo "<td>" . $row["Dept_Name"] . "</td>";
@@ -46,7 +40,7 @@ $Office_Phone= $_POST["dPhone"];
 			echo "<td>" . $row["Office_Phone"] . "</td>";
 			echo "</tr>"; //show results
 		}
-		echo '</table>'; 
+		echo '</table>';  
 	 
 	
 
