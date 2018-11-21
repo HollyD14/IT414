47f99e88ed3891c5f3c746b2e6d3bada329a1b14
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-21 14:43:02 -0600

    Update vehicle.php
    search function updated

--- vehicle.php
+++ vehicle.php
@@ -45,16 +45,179 @@
 			document.getElementById("createEmp").style.display="none";}
 		</script> 
 		</div>
+	<!--*****SEARCH VEHICLE*****-->
+<!--form-->
 	<div id="search">
 		<h1><center>Search Vehicles</center></h1>
-		<form method="POST" action="search.php"> <!--change form action to correct file name and change variables-->
-			<label>Vehicle No: </label><input type="text" id="dNo" name="dNo"> 
-			<label>Make: </label><input type="text" id="dName" name="dName">
-			<label>Model: </label><input type="text" id="dAddr" name="dAddr">
-			<label>Color: </label><input type="text" id="dPhone" name="dPhone">
-			<label>License Plate: </label><input type="text" id="dSuper" name="dSuper">
-			<input type="submit" id="searchButton" value="Search">
+		<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+			<label>Vehicle No: </label><input type="text" id="vNo" name="vNo"> 
+			<label>Make: </label><input type="text" id="make" name="make">
+			<label>Model: </label><input type="text" id="model" name="model">
+			<label>Year: </label><input type="text" id="y" name="y">
+			<label>Employee ID: </label><input type="text" id="eNo" name="eNo">
+			<input type="submit" id="searchButton" name="search" value="Search">
 	</div>
+<!--function-->
+<?php
+require 'open-db.php';
+if (isset($_POST['search'])){
+	if ($_POST['vNo']){
+		echo "<style>
+				.formDefault{
+					display: none;
+				} </style>";
+		$Vehicle_ID= $_POST['vNo'];
+		$Vid= mysqli_query($conn, "SELECT Emp_Number, Vehicle_ID, First_Name, Last_Name, Make, Model, Year, Color, Plate_Number FROM employee join vehicle on Vehicle_ID=V_ID where Vehicle_ID=$Vehicle_ID");
+		echo '<table>
+	<tr>
+		<th>Vehicle ID</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Year</th>
+		<th>Color</th>
+		<th>Plate Number</th>
+		<th>Assigned To</th>
+		<th></th>
+	</tr>';
+		while ($row = mysqli_fetch_array($Vid)) {
+			$id=$row['Vehicle_ID'];
+			echo "<tr>";
+			echo "<td>" . $row['Vehicle_ID'] . "</td>";
+			echo "<td>" . $row['Make'] . "</td>";
+			echo "<td>" . $row['Model'] . "</td>";
+			echo "<td>" . $row['Year'] . "</td>";
+			echo "<td>" . $row['Color'] . "</td>";
+			echo "<td>" . $row['Plate_Number'] . "</td>";
+			echo "<td>" . $row['First_Name'] . " " . $row['Last_Name']. "</td>";
+			echo "<td class='details'><a href='UpdateForm.php?id=$id'>Update</a></td>";
+		echo "</tr>";}
+	echo '</table>'; }
+	elseif ($_POST['make']){
+		echo "<style>
+				.formDefault{
+					display: none;
+				} </style>";
+		$Make= $_POST['make'];
+		$mk= mysqli_query($conn, "SELECT Emp_Number, Vehicle_ID, First_Name, Last_Name, Make, Model, Year, Color, Plate_Number FROM employee join vehicle on Vehicle_ID=V_ID where Make like '%$Make%'");
+		echo '<table>
+	<tr>
+		<th>Vehicle ID</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Year</th>
+		<th>Color</th>
+		<th>Plate Number</th>
+		<th>Assigned To</th>
+		<th></th>
+	</tr>';
+		while ($row = mysqli_fetch_array($mk)) {
+			$id=$row['Vehicle_ID'];
+			echo "<tr>";
+			echo "<td>" . $row['Vehicle_ID'] . "</td>";
+			echo "<td>" . $row['Make'] . "</td>";
+			echo "<td>" . $row['Model'] . "</td>";
+			echo "<td>" . $row['Year'] . "</td>";
+			echo "<td>" . $row['Color'] . "</td>";
+			echo "<td>" . $row['Plate_Number'] . "</td>";
+			echo "<td>" . $row['First_Name'] . " " . $row['Last_Name']. "</td>";
+			echo "<td class='details'><a href='UpdateForm.php?id=$id'>Update</a></td>";
+		echo "</tr>";}
+	echo '</table>';} 
+	elseif ($_POST['model']){
+		echo "<style>
+				.formDefault{
+					display: none;
+				} </style>";
+		$Model= $_POST['model'];
+		$md= mysqli_query($conn, "SELECT Emp_Number, Vehicle_ID, First_Name, Last_Name, Make, Model, Year, Color, Plate_Number FROM employee join vehicle on Vehicle_ID=V_ID where Model like '%$Model%'");
+		echo '<table>
+	<tr>
+		<th>Vehicle ID</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Year</th>
+		<th>Color</th>
+		<th>Plate Number</th>
+		<th>Assigned To</th>
+		<th></th>
+	</tr>';
+		while ($row = mysqli_fetch_array($md)) {
+			$id=$row['Vehicle_ID'];
+			echo "<tr>";
+			echo "<td>" . $row['Vehicle_ID'] . "</td>";
+			echo "<td>" . $row['Make'] . "</td>";
+			echo "<td>" . $row['Model'] . "</td>";
+			echo "<td>" . $row['Year'] . "</td>";
+			echo "<td>" . $row['Color'] . "</td>";
+			echo "<td>" . $row['Plate_Number'] . "</td>";
+			echo "<td>" . $row['First_Name'] . " " . $row['Last_Name']. "</td>";
+			echo "<td class='details'><a href='UpdateForm.php?id=$id'>Update</a></td>";
+		echo "</tr>";}
+	echo '</table>';} 
+	elseif ($_POST['y']){
+		echo "<style>
+				.formDefault{
+					display: none;
+				} </style>";
+		$Year= $_POST['y'];
+		$y= mysqli_query($conn, "SELECT Emp_Number, Vehicle_ID, First_Name, Last_Name, Make, Model, Year, Color, Plate_Number FROM employee join vehicle on Vehicle_ID=V_ID where Year like '%$Year%'");
+		echo '<table>
+	<tr>
+		<th>Vehicle ID</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Year</th>
+		<th>Color</th>
+		<th>Plate Number</th>
+		<th>Assigned To</th>
+		<th></th>
+	</tr>';
+			while ($row = mysqli_fetch_array($y)) {
+				$id=$row['Vehicle_ID'];
+			echo "<tr>";
+			echo "<td>" . $row['Vehicle_ID'] . "</td>";
+			echo "<td>" . $row['Make'] . "</td>";
+			echo "<td>" . $row['Model'] . "</td>";
+			echo "<td>" . $row['Year'] . "</td>";
+			echo "<td>" . $row['Color'] . "</td>";
+			echo "<td>" . $row['Plate_Number'] . "</td>";
+			echo "<td>" . $row['First_Name'] . " " . $row['Last_Name']. "</td>";
+			echo "<td class='details'><a href='UpdateForm.php?id=$id'>Update</a></td>";
+			echo "</tr>";}
+			echo "</table>";}	
+	elseif ($_POST['eNo']){
+		echo "<style>
+			.formDefault{
+				display: none;
+			} </style>";
+		$Emp_Number= $_POST['eNo'];
+		$eNo= mysqli_query($conn, "SELECT Emp_Number, Vehicle_ID, First_Name, Last_Name, Make, Model, Year, Color, Plate_Number FROM employee join vehicle on Vehicle_ID=V_ID where Emp_Number=$Emp_Number");
+		echo '<table>
+	<tr>
+		<th>Vehicle ID</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Year</th>
+		<th>Color</th>
+		<th>Plate Number</th>
+		<th>Assigned To</th>
+		<th></th>
+	</tr>';
+		while ($row = mysqli_fetch_array($eNo)) {
+			$id=$row['Vehicle_ID'];
+			echo "<tr>";
+			echo "<td>" . $row['Vehicle_ID'] . "</td>";
+			echo "<td>" . $row['Make'] . "</td>";
+			echo "<td>" . $row['Model'] . "</td>";
+			echo "<td>" . $row['Year'] . "</td>";
+			echo "<td>" . $row['Color'] . "</td>";
+			echo "<td>" . $row['Plate_Number'] . "</td>";
+			echo "<td>" . $row['First_Name'] . " " . $row['Last_Name']. "</td>";
+			echo "<td class='details'><a href='UpdateForm.php?id=$id'>Update</a></td>";
+		echo "</tr>";}
+	echo '</table>';} 
+			}
+?>		
 	
 		<!--Display all department info by default on main department page-->
 	<?php 
@@ -63,7 +226,7 @@
 	echo '<link rel="stylesheet" type = "text/css" href="../css/style.css">';
 	require 'open-db.php';
 	$result= mysqli_query($conn, "SELECT * FROM vehicle");
-	echo '<table>
+	echo '<div class = "formDefault"><table>
 	<tr>
 		<th>Vehicle ID</th>
 		<th>Make</th>
@@ -85,7 +248,7 @@
 		echo "<td>" . $row['Plate_Number'] . "</td>";
 		echo "<td class='details'><a href='UpdateVehicleForm.php?id=$id'>Update</a></td>";
 		echo "<td class='details'><a href='deleteFormVehicle.php?id=$id'>Delete</a></td>";
-		echo "</tr>";
+		echo "</tr></div>";
 	}
 	echo '</table>'; ?>	
 
