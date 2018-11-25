b4ff70d4dd5c6c80078fd571704d18b2798daa74
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-24 20:07:13 -0600

    Update addProj.php
    changed variable names, now goes back to main page and refreshes after add

--- workingfiles/php/addProj.php
+++ workingfiles/php/addProj.php
@@ -3,18 +3,25 @@
 require 'open-db.php';
 
 /*Add project*/
-$Proj_ID=$_POST['proj_ID'];
-$Proj_Name=$_POST['proj_Name'];
-$Start_Date=$_POST['s_Date'];
-$End_Date=$_POST['e_Date'];
+$Proj_ID=$_POST['pNo'];
+$Proj_Name=$_POST['pName'];
+$Start_Date=$_POST['sDate'];
+$End_Date=$_POST['eDate'];
 
 $sql = "INSERT into project (Project_ID, Project_Name, Start_Date, End_Date)
 VALUES ('$Proj_ID', '$Proj_Name', '$Start_Date','$End_Date')";
 
+//Following block returns the user to the updated project page when successful,
+//Or returns an error code with a shortcut to the project page if not
 if ($conn->query($sql) === TRUE) {
-   echo "Record inserted successfully";
+   echo "Record updated successfully";
+      echo "Return to Update Page: " ;
+	  header ("Location: /php/project.php");
 } else {
-   echo "Error inserting record: " . $conn->error;
+   echo "Error updating record: " . $conn->error;
+   echo <<<HTML
+			<a href="/php/project.php">Return to Project Page</a>
+HTML;
 }
-
 ?>
+
