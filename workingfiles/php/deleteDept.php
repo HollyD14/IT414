5b7a580571b80088590e49d913ee9bdd2e2a831e
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-24 20:15:37 -0600

    Update deleteDept.php
    goes back to main page and refreshes after delete

--- workingfiles/php/deleteDept.php
+++ workingfiles/php/deleteDept.php
@@ -2,20 +2,18 @@
 include 'open-db.php';
 include 'headerFooter.php';
 echo "<link href='../css/pageStyle.css' rel='stylesheet' type='text/css'/>";
-$dep_number=$_POST['dep_number'];
+$emp_number=$_POST['emp_number'];
 
-$sql = "DELETE from department WHERE Dept_ID=$dep_number";
+$sql = "DELETE from employee WHERE Emp_Number=$emp_number";
 
-//Following block returns the user to the updated department page when successful,
-//Or returns an error code with a shortcut to the department page if not
+//Following block returns the user to the updated employee page when successful,
+//Or returns an error code with a shortcut to the employee page if not
 if ($conn->query($sql) === TRUE) {
-   echo "Record updated successfully";
-      echo "Return to Update Page: " ;
-	  header ("Location: /php/department.php");
+	  header ("Location: /php/employee.php");
 } else {
    echo "Error updating record: " . $conn->error;
    echo <<<HTML
-			<a href="/php/department.php">Return to Department Page</a>
+			<a href="/php/employee.php">Return to Employee Page</a>
 HTML;
 }
 ?>
