40f062b27b364ef4c035e7fabed9f87f78e7346d
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-24 20:22:12 -0600

    Update deletePos.php
    goes back to main page and refreshes after delete

--- workingfiles/php/deletePos.php
+++ workingfiles/php/deletePos.php
@@ -6,14 +6,16 @@ $id=$_POST['id'];
 
 $sql = "DELETE from position WHERE Job_ID=$id";
 
-	if ($conn->query($sql) === TRUE) {
-		echo "<br><center><h3>Record deleted successfully</h3></center>";
-		echo "<br><center><a href='position.php'>Go back</a></center>";
-	} else {
-		echo "<br><center><h3>Error deleting record: " . $conn->error . "</h3></center>";
-		echo "<br><center><a href='position.php'>Go back</a></center>";
-	}
-	
-
-
+//Following block returns the user to the updated position page when successful,
+//Or returns an error code with a shortcut to the position page if not
+if ($conn->query($sql) === TRUE) {
+   echo "Record updated successfully";
+      echo "Return to Update Page: " ;
+	  header ("Location: /php/position.php");
+} else {
+   echo "Error updating record: " . $conn->error;
+   echo <<<HTML
+			<a href="/php/position.php">Return to Position Page</a>
+HTML;
+}
 ?>
