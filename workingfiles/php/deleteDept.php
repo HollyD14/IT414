1220383b8b53870784d01f47aea1f172202773fc
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-24 20:14:22 -0600

    Update deleteDept.php
    now goes back to main page and refreshes after delete

--- workingfiles/php/deleteDept.php
+++ workingfiles/php/deleteDept.php
@@ -6,11 +6,16 @@ $dep_number=$_POST['dep_number'];
 
 $sql = "DELETE from department WHERE Dept_ID=$dep_number";
 
-	if ($conn->query($sql) === TRUE) {
-		echo "<br><center><h3>Record deleted successfully</h3></center>";
-		echo "<br><center><a href='department.php'>Go back</a></center>";
-	} else {
-		echo "<br><center><h3>Error deleting record: " . $conn->error . "</h3></center>";
-		echo "<br><center><a href='department.php'>Go back</a></center>";
-	}
+//Following block returns the user to the updated department page when successful,
+//Or returns an error code with a shortcut to the department page if not
+if ($conn->query($sql) === TRUE) {
+   echo "Record updated successfully";
+      echo "Return to Update Page: " ;
+	  header ("Location: /php/department.php");
+} else {
+   echo "Error updating record: " . $conn->error;
+   echo <<<HTML
+			<a href="/php/department.php">Return to Department Page</a>
+HTML;
+}
 ?>
