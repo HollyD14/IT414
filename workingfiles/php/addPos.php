0a28adb5e2726ce871770c157e9a0cf4bedbb35a
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-24 20:04:52 -0600

    Update addPos.php
    changed variable names, now goes back to main page and refreshes after add

--- workingfiles/php/addPos.php
+++ workingfiles/php/addPos.php
@@ -3,18 +3,24 @@
 require 'open-db.php';
 
 /*Add position*/
-$Job_ID=$_POST['job_No'];
-$Job_Title=$_POST['job_Title'];
-$B_Sal=$_POST['b_Sal'];
+$Job_ID=$_POST['jNo'];
+$Job_Title=$_POST['jTitle'];
+$B_Sal=$_POST['base'];
 
 
 $sql = "INSERT into position (Job_ID, Job_Title, Base_Salary)
-VALUES ('$Job_ID', '$Job_Title', '$B_Sal',)";
+VALUES ('$Job_ID', '$Job_Title', '$B_Sal')";
 
+//Following block returns the user to the updated position page when successful,
+//Or returns an error code with a shortcut to the position page if not
 if ($conn->query($sql) === TRUE) {
-   echo "Record inserted successfully";
+   echo "Record updated successfully";
+      echo "Return to Update Page: " ;
+	  header ("Location: /php/position.php");
 } else {
-   echo "Error inserting record: " . $conn->error;
+   echo "Error updating record: " . $conn->error;
+   echo <<<HTML
+			<a href="/php/position.php">Return to Position Page</a>
+HTML;
 }
-
 ?>
