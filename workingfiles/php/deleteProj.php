725c065e4c4e1b8070e8a58d1f3442b81dc2c2ea
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-24 20:23:19 -0600

    Update deleteProj.php
    goes back to main page and refreshes after delete

--- workingfiles/php/deleteProj.php
+++ workingfiles/php/deleteProj.php
@@ -6,14 +6,16 @@ $id=$_POST['id'];
 
 $sql = "DELETE from project WHERE Project_ID=$id";
 
-	if ($conn->query($sql) === TRUE) {
-		echo "<br><center><h3>Record deleted successfully</h3></center>";
-		echo "<br><center><a href='project.php'>Go back</a></center>";
-	} else {
-		echo "<br><center><h3>Error deleting record: " . $conn->error . "</h3></center>";
-		echo "<br><center><a href='project.php'>Go back</a></center>";
-	}
-	
-
-
+//Following block returns the user to the updated project page when successful,
+//Or returns an error code with a shortcut to the project page if not
+if ($conn->query($sql) === TRUE) {
+   echo "Record updated successfully";
+      echo "Return to Update Page: " ;
+	  header ("Location: /php/project.php");
+} else {
+   echo "Error updating record: " . $conn->error;
+   echo <<<HTML
+			<a href="/php/project.php">Return to Project Page</a>
+HTML;
+}
 ?>
