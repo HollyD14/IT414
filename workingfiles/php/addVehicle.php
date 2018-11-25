2a8c2c3e18d8f4437b23aef56d51c7c9e315374e
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-24 20:09:39 -0600

    Update addVehicle.php
    changed variable names, now goes back to main page and refreshes after add

--- workingfiles/php/addVehicle.php
+++ workingfiles/php/addVehicle.php
@@ -3,21 +3,27 @@
 require 'open-db.php';
 
 /*Add vehicle*/
-$Vehicle_ID=$_POST['vehicle_ID'];
+$Vehicle_ID=$_POST['vNo'];
 $Make=$_POST['make'];
 $Model=$_POST['model'];
 $Year=$_POST['year'];
 $Color=$_POST['color'];
-$Plate_Num=$_POST['plate_num'];
+$Plate_Num=$_POST['pNumber'];
 
 
 $sql = "INSERT into vehicle (Vehicle_ID, Make, Model, Year, Color, Plate_Number)
-VALUES ('$Vehicle_ID', '$Proj_Name', '$Start_Date','$End_Date')";
+VALUES ('$Vehicle_ID', '$Make', '$Model','$Year', '$Color', '$Plate_Num')";
 
+//Following block returns the user to the updated department page when successfull,
+//Or returns an error code with a shortcut to the deapartment page if not
 if ($conn->query($sql) === TRUE) {
-   echo "Record inserted successfully";
+   echo "Record updated successfully";
+      echo "Return to Update Page: " ;
+	  header ("Location: /php/vehicle.php");
 } else {
-   echo "Error inserting record: " . $conn->error;
+   echo "Error updating record: " . $conn->error;
+   echo <<<HTML
+			<a href="/php/vehicle.php">Return to Vehicle Page</a>
+HTML;
 }
-
 ?>
