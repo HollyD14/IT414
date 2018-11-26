c062277ac53b0f0f30f4785120934896a324a517
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-25 20:03:26 -0600

    Update UpdatePayrollForm.php
    updated form style

--- workingfiles/php/UpdatePayrollForm.php
+++ workingfiles/php/UpdatePayrollForm.php
@@ -3,54 +3,9 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Your first HTML form, styled</title>
-    <style>
-      form {
-        /* Just to center the form on the page */
-        margin: 0 auto;
-        width: 400px;
-        /* To see the outline of the form */
-        padding: 1em;
-        border: 1px solid #CCC;
-        border-radius: 1em;
-      }
-      form div + div {
-        margin-top: 1em;
-      }
-      label {
-        /* To make sure that all labels have the same size and are properly aligned */
-        display: inline-block;
-        width: 200px;
-        text-align: left;
-      }
-      input, textarea {
-        /* To make sure that all text fields have the same font settings By default, textareas have a monospace font */
-        font: 1em sans-serif;
-        /* To give the same size to all text fields */
-        width: 300px;
-        box-sizing: border-box; /* To harmonize the look & feel of text field border */
-        border: 1px solid #999;
-      }
-      input:focus, textarea:focus {
-        /* To give a little highlight on active elements */
-        border-color: #000;
-      }
-      textarea {
-        /* To properly align multiline text fields with their labels */
-        vertical-align: top;
-        /* To give enough room to type some text */
-        height: 5em;
-      }
-      .button {
-        /* To position the buttons to the same position of the text fields */
-        padding-left: 90px;
-        /* same size as the label elements */
-      }
-      button {
-        /* This extra margin represent roughly the same space as the space between the labels and their text fields */
-        margin-left: .5em;
-      }
-    </style>
+    <title>Update Payroll</title>
+	<link href='../css/updateFormStyle.css' rel='stylesheet' type='text/css'/>
+    <link href="../css/pageStyle.css" rel="stylesheet" type="text/css"/>
 </head>
 
 
@@ -67,21 +22,25 @@ while ($row = mysqli_fetch_array($result)) {
 	$g = $row['Garnishments'];
 
 ?>
-	<div class="formPopup" id="updatePayroll"> 
+	<center><div class="formContainer" id="updatePayroll"> 
     <form action="updatePayroll.php" method="post">
+	<h2>Update Payroll</h2>
+        <center><div>
         <div>
-          <label for="number">Payroll ID:</label> <input type="text" id="Payroll_ID" name="Payroll_ID" value="<?php echo htmlspecialchars($p); ?>" readonly="readonly">
+          <label for="number">Payroll ID: <input type="text" id="Payroll_ID" name="Payroll_ID" value="<?php echo htmlspecialchars($p); ?>" readonly="readonly"></label>
         </div>
         <div>
-          <label for="salary">Salary: <i>(with or without commas)</i></label> <input type="text" id="Salary" name="Salary" value="<?php echo htmlspecialchars($s); ?>" required>
+          <label for="salary">Salary: <i>(with or without commas)</i> <input type="text" id="Salary" name="Salary" value="<?php echo htmlspecialchars($s); ?>" required></label>
         </div>
         <div>
-          <label for="garnish">Garnishments: <i>(with or without commas)</i></label> <input type="text" id="Garnishments" name="Garnishments" value="<?php echo htmlspecialchars($g); ?>">
+          <label for="garnish">Garnishments: <i>(with or without commas)</i> <input type="text" id="Garnishments" name="Garnishments" value="<?php echo htmlspecialchars($g); ?>"> </label>
         </div>
-        <div class="button">
-          <button type="submit">Submit</button>
-        </div>
+    <div class="btn">
+          <button class="save" type="submit">Update</button>
+		  </div></center>
     </form>
+	<button class="cancel" onclick="window.history.back()">Cancel</button>
+	</div></center>
 
 </html>
 <?php } ?>
