cc847d07dc2c824d6297d4ffe001012f159fc086
HollyD14 <44684665+HollyD14@users.noreply.github.com>
2018-11-25 20:01:21 -0600

    Update UpdateDepartmentForm.php
    updated form style

--- workingfiles/php/UpdateDepartmentForm.php
+++ workingfiles/php/UpdateDepartmentForm.php
@@ -3,54 +3,9 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Your first HTML form, styled</title>
-    <style>
-      form {
-        /* Just to center the form on the page */
-        margin: 0 auto;
-        width: 400px;
-        /* To see the outline of the form */
-        padding: 1em;
-        border: 1px solid #CCC;
-        border-radius: 1em;
-      }
-      form div + div {
-        margin-top: 1em;
-      }
-      label {
-        /* To make sure that all labels have the same size and are properly aligned */
-        display: inline-block;
-        width: 200px;
-        text-align: left;
-      }
-      input, textarea {
-        /* To make sure that all text fields have the same font settings By default, textareas have a monospace font */
-        font: 1em sans-serif;
-        /* To give the same size to all text fields */
-        width: 300px;
-        box-sizing: border-box; /* To harmonize the look & feel of text field border */
-        border: 1px solid #999;
-      }
-      input:focus, textarea:focus {
-        /* To give a little highlight on active elements */
-        border-color: #000;
-      }
-      textarea {
-        /* To properly align multiline text fields with their labels */
-        vertical-align: top;
-        /* To give enough room to type some text */
-        height: 5em;
-      }
-      .button {
-        /* To position the buttons to the same position of the text fields */
-        padding-left: 90px;
-        /* same size as the label elements */
-      }
-      button {
-        /* This extra margin represent roughly the same space as the space between the labels and their text fields */
-        margin-left: .5em;
-      }
-    </style>
+    <title>Update Department</title>
+	<link href='../css/updateFormStyle.css' rel='stylesheet' type='text/css'/>
+	<link href="../css/pageStyle.css" rel="stylesheet" type="text/css"/>
 </head>
 
 
@@ -66,24 +21,28 @@ while ($row = mysqli_fetch_array($result)) {
 	$a = $row['Office_Addr'];
 	$p = $row['Office_Phone'];	
 ?>
-	<div class="formPopup" id="updateDept"> 
+
+	<center><div class="formContainer" id="updateDept">	
     <form action="updateDept.php" method="post">
-        <div>
-          <label for="number">Department Number:</label> <input type="text" id="dep_number" name="dep_number" value="<?php echo htmlspecialchars($d); ?>" readonly="readonly">
+	<h2>Update Department</h2>
+        <center><div>
+          <label for="number">Department Number: <input type="text" id="dep_number" name="dep_number" value="<?php echo htmlspecialchars($d); ?>" readonly="readonly"></label>
         </div>
         <div>
-          <label for="name">Department Name:</label> <input type="text" id="dep_name" name="dep_name" value="<?php echo htmlspecialchars($n); ?>" required>
+          <label for="name">Department Name: <input type="text" id="dep_name" name="dep_name" value="<?php echo htmlspecialchars($n); ?>" required></label>
         </div>
         <div>
-		  <label for="address">Office Address:</label> <input    type="text" id="address" name="address" value="<?php echo htmlspecialchars($a); ?>">
+		  <label for="address">Office Address: <input    type="text" id="address" name="address" value="<?php echo htmlspecialchars($a); ?>"></label>
         </div>
         <div>
-          <label for="phone">Office Phone: <i>(with or without dashes)</i></label> <input type="text" id="phone" name="phone" value="<?php echo htmlspecialchars($p); ?>">
+          <label for="phone">Office Phone: <i>(with or without dashes)</i> <input type="text" id="phone" name="phone" value="<?php echo htmlspecialchars($p); ?>"></label>
         </div>
-        <div class="button">
-          <button type="submit">Submit</button>
-        </div>
+        <div class="btn">
+          <button class="save" type="submit">Update</button>
+		  </div></center>
     </form>
+	<button class="cancel" onclick="window.history.back()">Cancel</button>
+	</div></center>
 
 </html>
 <?php } ?>
