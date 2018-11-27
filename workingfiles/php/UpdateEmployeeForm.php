8a7ed8a44c0d610ae2344ae84fa1e65cfdc3739c
jenlynsea <44710672+jenlynsea@users.noreply.github.com>
2018-11-27 17:57:45 -0600

    Update UpdateEmployeeForm.php
    Finished update drop downs and figured out how to update salary table from employee form

--- workingfiles/php/UpdateEmployeeForm.php
+++ workingfiles/php/UpdateEmployeeForm.php
@@ -15,23 +15,39 @@
 $id=$_GET['id'];
 require 'open-db.php';
 	
-	$result= mysqli_query($conn, "SELECT Emp_Number, First_Name, Last_Name, Address, Birth_Date, Sex, SSN, Start_Date, J_ID, D_ID, V_ID, P_ID, Proj_ID FROM employee WHERE Emp_Number=$id");
+	$result= mysqli_query($conn, "SELECT Emp_Number, First_Name, Last_Name, Address, Birth_Date, Sex, SSN,
+										 employee.Start_Date as S_Date, position.Job_Title as J_Title, department.Dept_Name as D_Name,
+										 project.Project_Name as Proj_Name, payroll.Salary as P_Salary, vehicle.Plate_Number as V_Plate, J_ID, D_ID, V_ID, P_ID, Proj_ID  
+								  FROM employee 
+								  LEFT JOIN position ON position.Job_ID = employee.J_ID 
+								  LEFT JOIN department ON department.Dept_ID = employee.D_ID
+								  LEFT JOIN project ON project.Project_ID = employee.Proj_ID
+								  LEFT JOIN payroll ON payroll.Payroll_ID = employee.P_ID
+								  LEFT JOIN vehicle ON vehicle.Vehicle_ID = employee.V_ID
+								  WHERE Emp_Number=$id
+								  ORDER BY Emp_Number");
 	
-while ($row = mysqli_fetch_array($result)) {
-	
-	$e = $row['Emp_Number'];
-	$f = $row['First_Name'];
-	$l = $row['Last_Name'];
-	$a = $row['Address'];
-	$b = $row['Birth_Date'];
-	$s = $row['Sex'];
-	$n = $row['SSN'];
-	$sd = $row['Start_Date'];
-	$j = $row['J_ID'];
-	$d = $row['D_ID'];
-	$v = $row['V_ID'];
-	$p = $row['P_ID'];
-	$pr = $row['Proj_ID'];
+	while ($row = mysqli_fetch_array($result)) {
+		
+		$e = $row['Emp_Number'];
+		$f = $row['First_Name'];
+		$l = $row['Last_Name'];
+		$a = $row['Address'];
+		$b = $row['Birth_Date'];
+		$s = $row['Sex'];
+		$n = $row['SSN'];
+		$sd = $row['S_Date'];
+		$jt = $row['J_Title'];
+		$dn = $row['D_Name'];
+		$pn = $row['Proj_Name'];
+		$ps = $row['P_Salary'];
+		$plate = $row['V_Plate'];
+		$j = $row['J_ID'];
+		$d = $row['D_ID'];
+		$v = $row['V_ID'];
+		$p = $row['P_ID'];
+		$pr = $row['Proj_ID'];
+
 ?>
 
 	<center><div class="formContainer" id="updateEmployee"> 
@@ -78,17 +94,22 @@ while ($row = mysqli_fetch_array($result)) {
 		<div>
 		  <label for="J_ID">Position:	 
 		  <select id="J_ID" name="J_ID">
-		  <option selected="selected"><?php echo htmlspecialchars($j); ?></option>
+		  <option value=<?php echo htmlspecialchars($j); ?> selected="selected"><?php echo htmlspecialchars($jt); ?></option>
 		  <?php $sql= mysqli_query($conn, "SELECT Job_ID, Job_Title FROM position"); 
 		  while ($row = mysqli_fetch_array($sql)) {
 			echo '<option value="'.$row['Job_ID'] . '">' .$row['Job_Title'].'</option>'; }?>
 		  </select>
 		</div>	
 		
+        <div>
+		  <input type="hidden" id="P_ID" name="P_ID" value="<?php echo htmlspecialchars($p); ?>">
+		  <label for="P_Salary">Salary:</label> <input type="text" id="P_Salary" name="P_Salary" value="<?php echo htmlspecialchars($ps); ?>">
+        </div>
+		
 		<div>
 		  <label for="D_ID">Department:
 		  <select id="D_ID" name="D_ID">	
-		  <option selected="selected"><?php echo htmlspecialchars($d); ?></option>		  
+		  <option value=<?php echo htmlspecialchars($d); ?> selected="selected"><?php echo htmlspecialchars($dn); ?></option>
 		  <?php $sql= mysqli_query($conn, "SELECT Dept_ID, Dept_Name FROM department"); 
 		  while ($row = mysqli_fetch_array($sql)) {
 			echo '<option value="'.$row['Dept_ID'] . '">' . $row['Dept_Name'] .'</option>'; }?>
@@ -96,31 +117,22 @@ while ($row = mysqli_fetch_array($result)) {
 		</div>
 		
 		<div>
-		  <label for="V_ID">Vehicle:
-		  <select id="V_ID" name="V_ID">	
-		  <option selected="selected"><?php echo htmlspecialchars($v); ?></option>	
-		  <?php $sql= mysqli_query($conn, "SELECT Vehicle_ID, Plate_Number FROM vehicle"); 
+		  <label for="Proj_ID">Project:
+		  <select id="Proj_ID" name="Proj_ID">	
+		  <option value=<?php echo htmlspecialchars($pr); ?> selected="selected"><?php echo htmlspecialchars($pn); ?></option>
+		  <?php $sql= mysqli_query($conn, "SELECT Project_ID, Project_Name FROM project"); 
 		  while ($row = mysqli_fetch_array($sql)) {
-			echo '<option value="'.$row['Vehicle_ID'] .  '">' . $row['Plate_Number'] . '</option>';  }?>
+			echo '<option value="'.$row['Project_ID'] . '">' . $row['Project_Name'] .  '</option>';  }?>
 		  </select>	
 		</div>
 		
 		<div>
-		  <label for="P_ID">Payroll ID:
-		  <select id="P_ID" name="P_ID">	
-		  <?php $sql= mysqli_query($conn, "SELECT Payroll_ID, Salary FROM payroll"); 
+		  <label for="V_ID">Vehicle:
+		  <select id="V_ID" name="V_ID">	
+		  <option value=<?php echo htmlspecialchars($v); ?> selected="selected"><?php echo htmlspecialchars($plate); ?></option>
+		  <?php $sql= mysqli_query($conn, "SELECT Vehicle_ID, Plate_Number FROM vehicle"); 
 		  while ($row = mysqli_fetch_array($sql)) {
-			echo '<option value="'.$row['Payroll_ID'] . '">' . $row['Salary'] . '</option>'; }?>
-		  </select>	
-        </div>
-		
-		<div>
-		  <label for="Proj_ID">Project:
-		  <select id="Proj_ID" name="Proj_ID">	
-		  <option selected="selected"><?php echo htmlspecialchars($pr); ?></option>	
-		  <?php $sql= mysqli_query($conn, "SELECT Project_ID, Project_Name FROM project"); 
-		  while ($row = mysqli_fetch_array($sql)) {
-			echo '<option value="'.$row['Project_ID'] . '">' . $row['Project_Name'] .  '</option>';  }?>
+			echo '<option value="'.$row['Vehicle_ID'] .  '">' . $row['Vehicle_ID'] . "-" . $row['Plate_Number'] . '</option>';  }?>
 		  </select>	
 		</div>
 		
@@ -134,5 +146,4 @@ while ($row = mysqli_fetch_array($result)) {
 
 </html>
 
-<?php } 
-	?>
+<?php } ?>
