6581e9bd2c75073b3ee15ea6e78a500bc5f96ad6
jenlynsea <44710672+jenlynsea@users.noreply.github.com>
2018-11-27 11:40:14 -0600

    Update addEmp.php
    Added the code to accept blanks for the employee attributes

--- workingfiles/php/addEmp.php
+++ workingfiles/php/addEmp.php
@@ -6,19 +6,18 @@ require 'open-db.php';
 $E_ID=$_POST['eNo'];
 $E_FName=$_POST['eFName'];
 $E_LName=$_POST['eLName'];
-$BDate=$_POST['bdate'];
 $Sex=$_POST['sex'];
+$BDate=$_POST['bdate'];
 $E_Addr=$_POST['eAddr'];
 $SSN=$_POST['ssn'];
 $Start_Date=$_POST['startDate'];
 
 
-
-$Emp = "INSERT into employee (Emp_Number, First_Name, Last_Name, Birth_Date, Sex, Address, SSN, 
-													   Start_Date)
-VALUES ('$E_ID', '$E_FName', '$E_LName', '$BDate', '$Sex', '$E_Addr', '$SSN', '
-				 $Start_Date')";
-
+$Emp = "INSERT into employee (Emp_Number, First_Name, Last_Name, Birth_Date, Sex, Address, SSN, Start_Date)
+VALUES ('$E_ID', '$E_FName', '$E_LName', ".(($BDate=='')? "NULL" :("'".$BDate."'")).",
+		".(($Sex=='')? "NULL" :("'".$Sex."'")).", ".(($E_Addr=='')?"NULL":("'".$E_Addr."'")).",
+		".(($SSN=='')?"NULL":("'".$SSN."'")).", ".(($Start_Date=='')?"NULL":("'".$Start_Date."'"))."
+	   )";
 
 //Following block returns the user to the updated department page when successfull,
 //Or returns an error code with a shortcut to the deapartment page if not
